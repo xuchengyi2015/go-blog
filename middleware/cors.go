39897c0c62609e1728f89cb94f298ee759f1b99e
xuchengyi2015 <xuchengyi2015@gmail.com>
2019-08-24 11:25:08 +0800

    cors config

--- middleware/cors.go
+++ middleware/cors.go
@@ -3,14 +3,20 @@ package middleware
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"os"
+	"strings"
 )
 
 // Cors 跨域配置
 func Cors() gin.HandlerFunc {
+	godotenv.Load()
+	corsDomain := os.Getenv("CORS_DOMIAN")
+
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
-	config.AllowOrigins = []string{"http://localhost:8080", "http://localhost:3001", "http://192.168.2.3"}
+	config.AllowOrigins = strings.Split(corsDomain, "|")
 	config.AllowCredentials = true
 	return cors.New(config)
 }
